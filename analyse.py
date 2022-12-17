8f5a4b053505d0c31285917c6c22f946c012d0f7
Bernhard Utz <if21b241@technikum-wien.at>
2022-12-17 12:34:42 +0100

    refactor

--- analyse.py
+++ analyse.py
@@ -6,7 +6,19 @@ import math
 import sys
 
 
-def getParams(img):
+def getMidPoint(p1, p2):
+    return [float((p1[0] + p2[0]) / 2), float((p1[1] + p2[1]) / 2)]
+
+
+def calcThickness(img, direction, middle, length):
+    # direction in +/- 90° weiter gehen bis wir auf ein schwarzes pixel treffen
+    # a^2 + b^2 = c^2
+    a = float(length/2)
+    b = float(length/2)
+    c = float(numpy.sqrt(numpy.square(a) + numpy.square(b)))
+
+
+def calcPoints(img):
     height, width, channels = img.shape  # BGR
 
     # Arrays with furthest points
@@ -35,14 +47,28 @@ def getParams(img):
               float((topLeft[1] + topRight[1]) / 2)]
     midBottom = [float((bottomLeft[0] + bottomRight[0]) / 2),
                  float((bottomLeft[1] + bottomRight[1]) / 2)]
-    direction = math.atan(
-        (midTop[1] - midBottom[1])/(midTop[0] - midBottom[0])) * 180 / math.pi
+    midMiddle = getMidPoint(midTop, midBottom)
+    print("MidTop: ", midTop)
+    print("MidBottom: ", midBottom)
+    print("MidMiddle: ", midMiddle)
+    return midTop, midBottom, midMiddle
+
+
+def getParams(img):
+    # diese Funktion muss noch verbessert werden -> genauer werden
+    top, bottom, middle = calcPoints(img)
+
+    direction = ((math.atan(
+        (top[1] - bottom[1])/(top[0] - bottom[0])) * 180 / math.pi) + 180) % 360  # + 180 um 0 grad anders zu definieren
     print("Direction: ", direction)
     length = numpy.sqrt(numpy.square(
-        midTop[0] - midBottom[0]) + numpy.square(midTop[1] - midBottom[1]))
+        top[0] - bottom[0]) + numpy.square(top[1] - bottom[1]))
     print("Length: ", length)  # +/- 3 Pixel bis jetzt bei meinen 2 Tests
-    start = (int(midTop[1]), int(midTop[0]))
-    end = (int(midBottom[1]), int(midBottom[0]))
+
+    thickness = calcThickness(img, direction, middle, length)
+
+    start = (int(top[1]), int(top[0]))
+    end = (int(bottom[1]), int(bottom[0]))
 
     # show line
     cv2.line(img, start, end, (255, 0, 255), 1)
@@ -53,12 +79,13 @@ def getParams(img):
 def checkFragments():
     directory = "fragments"
     for filename in os.listdir(directory):
+        print()
         print(filename)
         f = os.path.join(directory, filename)
         # checking if it is a file
-        if os.path.isfile(f):
+        img = cv2.imread(f)
+        if os.path.isfile(f) and img is not None:
             # libpng warning: iCCP: known incorrect sRGB profile
-            img = cv2.imread(f)
             getParams(img)
 
 
