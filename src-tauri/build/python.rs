735811041760108042fa22f11f53854422e98f91
Leonard Struck <me@leonardstruck.com>
2023-05-08 08:58:34 +0200

    compile python with nuitka

--- src-tauri/build/python.rs
+++ src-tauri/build/python.rs
@@ -26,7 +26,34 @@ impl Builder {
         // tell cargo to rerun build script if vendor directory changes
         println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
 
-        self.paths.push(path);
+        let name = path.file_name().unwrap().to_str().unwrap();
+
+        // check if binary already exists
+        let bin_dir = crate::utils::get_bin_dir();
+        if bin_dir.join(name).exists() {
+            // check if binary is up to date
+            let bin_metadata = std::fs::metadata(bin_dir.join(name)).unwrap();
+
+            // iterate over all files in vendor directory
+            for entry in std::fs::read_dir(path.clone()).unwrap() {
+                let entry = entry.unwrap();
+                let file_path = entry.path();
+
+                // check if path is a file
+                if file_path.is_file() {
+                    // check if file is newer than binary
+                    let metadata = std::fs::metadata(file_path.clone()).unwrap();
+                    if metadata.modified().unwrap() > bin_metadata.modified().unwrap() {
+                        // rebuild binary
+                        self.paths.push(path);
+                        return;
+                    }
+                }
+            }
+        } else {
+            // build binary
+            self.paths.push(path);
+        }
     }
 
     fn resolve_requirements(&self) {
@@ -66,7 +93,8 @@ impl Builder {
         let resource_dir = crate::utils::get_bin_dir();
 
         // copy all folders and files recursively to the target directory
-        let folders = std::fs::read_dir(self.out_dir.clone().join("python")).unwrap();
+        let folders =
+            std::fs::read_dir(self.out_dir.clone().join("python").join("main.dist")).unwrap();
 
         for folder in folders {
             let folder = folder.unwrap();
@@ -78,7 +106,7 @@ impl Builder {
                 fs_extra::file::copy(
                     path.clone(),
                     resource_dir.clone().join(path.file_name().unwrap()),
-                    &fs_extra::file::CopyOptions::new().skip_exist(true),
+                    &fs_extra::file::CopyOptions::new().overwrite(true),
                 )
                 .unwrap();
                 continue;
@@ -88,7 +116,7 @@ impl Builder {
             fs_extra::dir::copy(
                 path,
                 resource_dir.clone(),
-                &fs_extra::dir::CopyOptions::new().skip_exist(true),
+                &fs_extra::dir::CopyOptions::new().overwrite(true),
             )
             .unwrap();
         }
@@ -103,182 +131,35 @@ fn build_python_projects(paths: Vec<std::path::PathBuf>) {
     // create target directory if it doesn't exist
     std::fs::create_dir_all(target_dir.clone()).unwrap();
 
-    // create new spec file
-    let spec_file = target_dir.join("python.spec");
-
-    // delete old spec file
-    std::fs::remove_file(spec_file.clone()).unwrap_or(());
-
-    // create new spec file
-    std::fs::File::create(spec_file.clone()).unwrap();
-
-    let mut spec_content = String::new();
-
-    // append spec head to spec_content
-    spec_content.push_str(
-        r#"# -*- mode: python ; coding: utf-8 -*-
-    
-block_cipher = None
-"#,
-    );
-    spec_content.push_str("\n\n");
-
-    // create analysis script
-    for path in paths.clone() {
-        let path_binding = path.clone();
-        let target_name = path_binding.file_name().unwrap().to_str().unwrap();
-
-        // append to spec file
-        spec_content.push_str(
-            format!(
-                r#"{} = Analysis(['{}'],
-                pathex=['{}'],
-                binaries=[],
-                datas=[],
-                hiddenimports=[],
-                hookspath=[],
-                hooksconfig=[],
-                runtime_hooks=[],
-                excludes=[],
-                win_no_prefer_redirects=False,
-                win_private_assemblies=False,
-                cipher=block_cipher,
-                noarchive=False
-                )
-            "#,
-                target_name,
-                path.join("main.py").to_str().unwrap().replace('\\', "/"),
-                path.to_str().unwrap().replace('\\', "/")
-            )
-            .as_str(),
-        );
-    }
-
-    spec_content.push('\n');
-
-    // merge scripts
-    spec_content.push_str("MERGE ( ");
-    for path in paths.clone() {
-        let path_binding = path.clone();
-        let target_name = path_binding.file_name().unwrap().to_str().unwrap();
-
-        // append to spec file
-        spec_content
-            .push_str(format!(r#"( {target_name}, '{target_name}', '{target_name}' )"#).as_str());
-        // append comma if not last element
-        if path != *paths.last().unwrap() {
-            spec_content.push_str(", ");
-        }
-    }
-    spec_content.push_str(" )\n\n");
-
-    // create pyz script
-    for path in paths.clone() {
-        let path_binding = path.clone();
-        let target_name = path_binding.file_name().unwrap().to_str().unwrap();
-
-        // append to spec file
-        spec_content.push_str(
-            format!(
-                r#"{target_name}_pyz = PYZ({target_name}.pure)
-            "#
-            )
-            .as_str(),
-        );
-    }
-
-    // create executables
-    for path in paths.clone() {
-        let path_binding = path.clone();
-        let target_name = path_binding.file_name().unwrap().to_str().unwrap();
-
-        // append to spec file
-        spec_content.push_str(
-            format!(
-                r#"
-{target_name}_exe = EXE(
-    {target_name}_pyz,
-    {target_name}.scripts,
-    [],
-    exclude_binaries=True,
-    name='{target_name}',
-    debug=False,
-    bootloader_ignore_signals=False,
-    strip=False,
-    upx=True,
-    console=True,
-    disable_windowed_traceback=False,
-    argv_emulation=False,
-    target_arch=None,
-    codesign_identity=None,
-    entitlements_file=None,
-)
-            "#
-            )
-            .as_str(),
-        );
-    }
-
-    // collect
-    spec_content.push_str(
-        r#"
-coll = COLLECT("#,
-    );
-    for path in paths.clone() {
-        let path_binding = path.clone();
-        let target_name = path_binding.file_name().unwrap().to_str().unwrap();
-
-        // append to spec file
-        spec_content.push_str(
-            format!(
-                r#"
-    {target_name}_exe,
-    {target_name}.binaries,
-    {target_name}.zipfiles,
-    {target_name}.datas,"#,
-            )
-            .as_str(),
-        );
-
-        // append comma if not last element
-        if path != *paths.last().unwrap() {
-            spec_content.push_str(", ");
-        }
-    }
-
-    spec_content.push_str(
-        r#"
-    strip=False,
-    upx=True,
-    upx_exclude=[],
-    name='python'
-)
-            "#,
-    );
-
-    // write out spec_content to spec_file
-    std::fs::write(spec_file.clone(), spec_content).unwrap();
-
     // build python projects
-    let mut command = Command::new("python");
-    command
-        .arg("-m")
-        .arg("PyInstaller")
-        .arg("--distpath")
-        .arg(target_dir.clone())
-        .arg("--workpath")
-        .arg(target_dir.join("build"))
-        .arg("-y")
-        .arg(spec_file);
 
-    // wait for command to finish
-    let output = command.output().unwrap();
+    for path in paths {
+        let name = path.file_name().unwrap().to_str().unwrap();
+        let output = Command::new("python")
+            .arg("-m")
+            .arg("nuitka")
+            .arg("--static-libpython=no")
+            .arg("--standalone")
+            .arg(format!("--output-filename={}", name))
+            .arg(format!(
+                "--output-dir={}",
+                target_dir.join("python").to_str().unwrap()
+            ))
+            .arg("main.py")
+            .current_dir(path)
+            .output();
 
-    // check if command was successful
-    if !output.status.success() {
-        panic!(
-            "Failed to build python projects: {}",
-            String::from_utf8(output.stderr).unwrap()
-        );
+        let output = match output {
+            Ok(output) => output,
+            Err(err) => panic!("Failed to build python project: {}", err),
+        };
+
+        // check if command was successful
+        if !output.status.success() {
+            panic!(
+                "Failed to build python project: {}",
+                String::from_utf8(output.stderr).unwrap()
+            );
+        }
     }
 }
